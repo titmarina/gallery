6da2df24b1caed67414c04ede44a127b529d18c8
Marina Titova <marina-titova-1992@mail.ru>
2022-08-01 13:57:15 +0300

    slider-1

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,39 @@
-const swiper = new Swiper('.swiper', {
+new Swiper('.gallery__swiper', {
   // Optional parameters
-  direction: 'horizontal',
   loop: true,
+  direction: 'horizontal',
+  slidesPerView: 4,
+  spaceBetween: 30,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+          slidesPerView: 4,
+          spaceBetween: 40
+      }
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next-2',
+      prevEl: '.swiper-button-prev-2',
   },
+  pagination: {
+    el:'.swiper-pagination',
+    type: 'fraction',
+    clickable: true,
+    centereaSlides: true,
+}
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+
 });
