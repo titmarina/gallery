5eb71afa63e7061cc1880c1bdef91247d0fa0d86
Marina Titova <marina-titova-1992@mail.ru>
2022-08-02 15:49:42 +0300

    slider-gallery

--- js/slider.js
+++ js/slider.js
@@ -1,39 +1,40 @@
 new Swiper('.gallery__swiper', {
-  // Optional parameters
-  loop: true,
-  direction: 'horizontal',
-  slidesPerView: 4,
-  spaceBetween: 30,
-
-  breakpoints: {
-      // when window width is >= 320px
-      320: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-      },
-      // when window width is >= 480px
-      480: {
-          slidesPerView: 3,
-          spaceBetween: 30
-      },
-      // when window width is >= 640px
-      640: {
-          slidesPerView: 4,
-          spaceBetween: 40
-      }
-  },
-
-  // Navigation arrows
-  navigation: {
-      nextEl: '.swiper-button-next-2',
-      prevEl: '.swiper-button-prev-2',
-  },
-  pagination: {
-    el:'.swiper-pagination',
-    type: 'fraction',
-    clickable: true,
-    centereaSlides: true,
-}
+    // Optional parameters
+    loop: true,
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
 
 
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next-2',
+        prevEl: '.swiper-button-prev-2',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    }
 });
+
+
+
