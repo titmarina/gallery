438714e88b816f4ee579c69fad34a1b5c1ff59a1
Marina Titova <marina-titova-1992@mail.ru>
2022-08-16 23:53:55 +0300

    mobi

--- js/slider.js
+++ js/slider.js
@@ -9,20 +9,16 @@ new Swiper('.gallery__swiper', {
 
 
     breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        // when window width is >= 480px
-        480: {
+        // when window width is >= 360px
+        //360: {
+       //     slidesPerView: 2,
+       //     spaceBetween: 16,
+      //  },
+      
+        // when window width is >= 768px
+      768: {
             slidesPerView: 3,
-            spaceBetween: 30
-        },
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 4,
-            spaceBetween: 40
+           spaceBetween: 26
         }
     },
 
